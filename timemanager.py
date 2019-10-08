1688b098eadc9143afb6197b2dd3e769db737e84
leoneft <nefyodovleo@gmail.com>
2019-10-08 23:27:06 +0300

    pomenyal file

--- timemanager.py
+++ timemanager.py
@@ -2,7 +2,7 @@ import time
 import shelve
 
 while True:
-    time.sleep(60)
+    time.sleep(24 * 60 * 60)
     db = shelve.open('db', writeback=True)
     for user in db:
         db[user]['balance'] += db[user]['num_of_apps'] * 25
